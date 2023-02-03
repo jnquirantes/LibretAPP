a17bd2d06829661da1d66e2c4ecbbc7bae7ec1e5
Jnquirantes <jnquirantes@gmail.com>
2023-02-03 21:32:12 +0100

    Segunda entrega

--- app/src/main/java/com/app/programacion_multimedia/MainActivity.java
+++ app/src/main/java/com/app/programacion_multimedia/MainActivity.java
@@ -1,14 +1,19 @@
 package com.app.programacion_multimedia;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.airbnb.lottie.LottieAnimationView;
@@ -110,5 +115,22 @@ public class MainActivity extends AppCompatActivity {
                break;
          }
        });
+    }
+
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        new AlertDialog.Builder(this)
+                .setTitle("¿Realmente desea cerrar la aplicación?")
+                .setCancelable(true)
+                .setNegativeButton(android.R.string.cancel, null)
+                .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {// un listener que al pulsar, cierre la aplicacion
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        android.os.Process.killProcess(android.os.Process.myPid()); //Su funcion es algo similar a lo que se llama cuando se presiona el botón "Forzar Detención" o "Administrar aplicaciones", lo cuál mata la aplicación
+                        //finish(); Si solo quiere mandar la aplicación a segundo plano
+                    }
+                }).show();
+        return super.onKeyDown(keyCode, event);
     }
 }
